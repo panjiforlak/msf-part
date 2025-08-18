d9037da0314ced801069a19032d08af4e21ef763
panji <panjiforlak@gmail.com>
2025-08-18 14:57:17 +0700

    updated

--- src/modules/form_order/dto/create.dto.ts
+++ src/modules/form_order/dto/create.dto.ts
@@ -11,18 +11,34 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { enumFormOrderStatus } from '../entities/formOrder.entity';
 
 export class CreateFormOrderDto {
+  @ApiProperty({
+    description: 'ID inventory yang terkait dengan form order',
+    example: 1,
+  })
   @IsNumber()
   @IsNotEmpty()
   inventory_id?: number;
 
+  @ApiProperty({
+    description: 'Nomor form order',
+    example: 'IN-1HGBH41JXMN1918',
+  })
   @IsString()
   @IsNotEmpty()
   form_order_number?: string;
 
+  @ApiProperty({
+    description: 'Jumlah item yang dipesan',
+    example: 10,
+  })
   @IsNumber()
   @IsNotEmpty()
   quantity?: number;
 
+  @ApiProperty({
+    description: 'Status dari form order',
+    example: 'ENUM => ordered | pending | packing | in-order | finished',
+  })
   @IsNotEmpty()
   @IsEnum(enumFormOrderStatus)
   status?: enumFormOrderStatus;
